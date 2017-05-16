e732abb7eb5baa59574770ce010ea751f777c81c
seifElislam <seif.eleslam1990@gmail.com>
2017-05-16 13:52:12 +0200

    add segment for home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,15 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  goHome(){
+    this.navCtrl.push(ListPage)
+  }
+  goForRequset(){
+    this.navCtrl.push(ListPage)
+  }
+  goToScan(){
+    this.navCtrl.push(ListPage)
   }
 
 }
